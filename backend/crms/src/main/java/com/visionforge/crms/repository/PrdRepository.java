86da9d5c7cfd6a5e9e6399288831a5340c3c1413
Januka Karunakaran <karunakaranj.23@uom.lk>
2026-02-12 15:04:21 +0530

    Delete backend directory

--- backend/crms/src/main/java/com/visionforge/crms/repository/PrdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.visionforge.crms.repository;
-
-import com.visionforge.crms.model.Prd;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface PrdRepository extends MongoRepository<Prd, String> {
-    List<Prd> findByProjectId(String projectId);
-}
