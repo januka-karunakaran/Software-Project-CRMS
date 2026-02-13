39d2058fc0b844a044106110400b874d4cf6fa27
NooraRafiuddeen <noorafathi01@gmail.com>
2026-02-13 13:34:59 +0530

    Updated files to fix error

--- apps/company-portal/Frontend/.next/types/app/layout.ts
+++ apps/company-portal/Frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\asus\just test\company-frontend\app\layout.jsx
+// File: C:\Users\dfell\Desktop\Software project - CRMS\Project frontend coding\Software-Project-CRMS\apps\company-portal\frontend\app\layout.jsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
